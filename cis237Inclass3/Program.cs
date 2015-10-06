c9e78c5b8e7daeebabb5a42eabf07c0204733448
Yihan Wang <ywang4241@mail.kvcc.edu>
2015-10-06 15:24:34 -0400

    Poly

--- cis237Inclass3/Program.cs
+++ cis237Inclass3/Program.cs
@@ -31,13 +31,68 @@ namespace cis237Inclass3
             Console.WriteLine(hourlyEmployee.CalculateWeeklyGross());
 
 
-
     // can't make instance of abstract class
-//            Employee employee = new Employee("D", "B", "d");
+           // Employee employee = new Employee("D", "B", "d");
 
 
 
             Console.WriteLine(hourlyEmployee.GetDepartmentTwice());
+
+
+
+            Console.WriteLine("**************");
+
+
+            //Declare and instanciate one of each type of employee
+            HourlyEmployee joe = new HourlyEmployee("Joe", "Somebody","HR", 9.37m);
+            joe.HourlyRate = 87;
+
+            SalaryEmployee dave = new SalaryEmployee("Dave","Barnes","IT",150000m);
+
+
+            Employee spock = new SalaryEmployee("Mr.", "Spock", "ST", 0m);
+
+
+            IEmployee newPerson = new HourlyEmployee("Some","OneElse","WK", 12.34m);
+
+            ((HourlyEmployee)newPerson).HoursWorked = 21;
+
+
+            //Create an array of type IEmployee, and pass in the two instances we created.
+            //IEmployee can contain instances that are lower on the inheritance chain.
+            //(More specific imploementations)
+            //Add another on the fly. No need to assign it to a seperate variable first.
+            IEmployee[] employeeList = { 
+                                           joe,
+                                           dave,
+                                           new SalaryEmployee("James","Kirk","ST", 0m),
+                                           spock
+                                       };
+            
+            //Now we can loop through the list of employees and call the CalculateWeeklyGross
+            //On each of the employees. The program will be smart enough to know which 
+            //method to call depending on the current instance it is working with.
+
+            foreach(IEmployee employee in employeeList)
+            {
+                Console.WriteLine(employee.ToString());
+                Console.WriteLine(employee.CalculateWeeklyGross());
+                Console.WriteLine();
+            }
+
+
+            IEmployee[] anotherEmployeeList;
+            anotherEmployeeList = new IEmployee[20];
+
+            anotherEmployeeList[0] = joe;
+            anotherEmployeeList[1] = dave;
+
+            foreach (IEmployee employee in anotherEmployeeList)
+            {
+                Console.WriteLine(employee.ToString());
+                Console.WriteLine(employee.CalculateWeeklyGross());
+                Console.WriteLine();
+            }
 
 
         }
